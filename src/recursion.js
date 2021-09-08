1cd7e62303f0415c373075f0af9d1f5ccae5f0b9
Alex Kim <riftonee@gmail.com>
2021-09-08 12:18:22 -0700

    Completed Recursion!

--- src/recursion.js
+++ src/recursion.js
@@ -7,31 +7,118 @@
 // Example: 5! = 5 x 4 x 3 x 2 x 1 = 120
 // factorial(5); // 120
 var factorial = function(n) {
+  if (n < 0) {
+    return null;
+  }
+  if (n === 0) {
+    return 1;
+  }
+  return n * factorial(n-1);
 };
 
 // 2. Compute the sum of an array of integers.
 // sum([1,2,3,4,5,6]); // 21
 var sum = function(array) {
+
+  if (array.length === 0) {
+    return 0;
+  }
+
+  if (array.length === 1) {
+    return array[0];
+  }
+
+  return array[0] + sum(array.slice(1));
+
 };
 
 // 3. Sum all numbers in an array containing nested arrays.
 // arraySum([1,[2,3],[[4]],5]); // 15
 var arraySum = function(array) {
+  var result = 0;
+
+  // Base case : if not an array, return that number
+  if (!Array.isArray(array)) {
+    return array;
+  }
+
+  // Recursive function : use
+  array.forEach(function(item) {
+    result = result + arraySum(item)
+  });
+  return result;
 };
 
 // 4. Check if a number is even.
 var isEven = function(n) {
+  var direction;
+  if (n < 0) {
+    direction = -2;
+  } else {
+    direction = 2
+  }
+
+  // Base case : n = 0, true
+  if (n === 0) {
+    return true;
+  } if (n === 1 ) {
+    return false;
+  }
+
+  // Recursive function : subtract by two, if answer is 1, return false
+  var counter = n - direction;
+  if (isEven(counter)) {
+    return true;
+  } else {
+    return false;
+  }
+
 };
 
 // 5. Sum all integers below a given integer.
 // sumBelow(10); // 45
 // sumBelow(7); // 21
 var sumBelow = function(n) {
+  result = 0;
+  var direction;
+  if (n < 0) {
+    direction = -1;
+  } else {
+    direction = 1;
+  }
+
+  if (n === 0) {
+    return 0;
+  } else {
+    return result = n - direction + sumBelow(n - direction);
+  }
+
+
+
+
 };
 
 // 6. Get the integers within a range (x, y).
 // range(2,9); // [3,4,5,6,7,8]
 var range = function(x, y) {
+  var direction;
+  if (x > y) {
+    direction = -1;
+  } else {
+    direction = 1;
+  }
+
+  if (x === y) {
+    return [];
+  }
+  if (x + direction - y === 0) {
+    return [];
+  } else {
+    var numbers = range(x + direction, y);
+    numbers.unshift(x + direction);
+  }
+
+  return numbers;
 };
 
 // 7. Compute the exponent of a number.
@@ -40,6 +127,38 @@ var range = function(x, y) {
 // exponent(4,3); // 64
 // https://www.khanacademy.org/computing/computer-science/algorithms/recursive-algorithms/a/computing-powers-of-a-number
 var exponent = function(base, exp) {
+  var result;
+
+
+  var direction;
+
+  if (exp < 0) {
+    direction = -1;
+  } else {
+    direction = 1;
+  }
+
+
+  // Edge Case
+  if (base === 0) {
+    return 1;
+  }
+  if (exp === 0) {
+    return 1;
+  }
+  // Base Case
+  //if (exp === 1) {
+  //  return base;
+  //}
+  // Recursive Function
+  if (exp > 0) {
+    return result = base * exponent(base, (exp - direction))
+  }
+  if (exp < 0) {
+    return result =  exponent(base, (exp - direction)) / base;
+
+  }
+
 };
 
 // 8. Determine if a number is a power of two.
@@ -51,10 +170,18 @@ var powerOfTwo = function(n) {
 
 // 9. Write a function that reverses a string.
 var reverse = function(string) {
+  if (string.length <=1) {
+    return string;
+  }
+  else {
+    return string.charAt(string.length - 1) + reverse(string.substring(0, string.length - 1));
+  }
+
 };
 
 // 10. Write a function that determines if a string is a palindrome.
 var palindrome = function(string) {
+
 };
 
 // 11. Write a function that returns the remainder of x divided by y without using the
@@ -136,6 +263,19 @@ var countKeysInObj = function(obj, key) {
 // countValuesInObj(obj, 'r') // 2
 // countValuesInObj(obj, 'e') // 1
 var countValuesInObj = function(obj, value) {
+  var num = 0;
+  for (var prop in obj) {
+    if (prop === value) {
+      num ++;
+    }else{
+      var thing = obj[prop];
+      if (typeof thing === 'object') {
+        num += countValuesInObj(thing, value);
+      }
+    }
+
+  }
+  return num;
 };
 
 // 24. Find all keys in an object (and nested objects) by a provided name and rename
